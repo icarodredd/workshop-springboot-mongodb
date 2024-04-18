3e3f5f7a2f5abb5d18ebd779e5337f87f5fddd22
icarodredd <icarog854@gmail.com>
2024-04-18 14:28:46 -0300

    delete user

--- src/main/java/com/workshop/services/UserService.java
+++ src/main/java/com/workshop/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.domain.User;
+import com.workshop.dto.UserDto;
 import com.workshop.exception.ObjectNoutFoundException;
 import com.workshop.repositories.UserRepository;
 
@@ -22,8 +23,19 @@ public class UserService {
 
 	public User findById(String id) {
 		User user = userRepository.findById(id).get();
-		if (user == null) throw new ObjectNoutFoundException("Object not found");
+		if (user == null)
+			throw new ObjectNoutFoundException("Object not found");
 		return user;
+
+	}
+
+	public User insert(User user) {
+		return userRepository.insert(user);
+
+	}
+
+	public User fromDto(UserDto user) {
+		return new User(user.getId(), user.getName(), user.getEmail());
 
 	}
 
